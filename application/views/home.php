dd75fcf3d85c53fffe15e7c178bc9981ac21276d
Tapha Ngum <tapha@live.co.uk>
2010-06-17 02:31:55 +0300

    new css file

--- application/views/home.php
+++ application/views/home.php
@@ -34,8 +34,27 @@
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />	
 	
 	
-</head>		
-<body id="container">
+</head>	
+<div id ='header'>
+<img id='icon_no_1' src='/wahalu/assets/images/icon/w/wahalu_icon_w.png'>
+<div id ='nav'>
+<h1>
+<span id ='real_nav'>
+<a id='what' href='/'>What Is Wahalu?</a>
+<a id='why' href='/'>Benefits</a>
+<a id='pricing' href='/'>Pricing</a>
+<a id='how' href='/'>How To Buy</a>
+<a id='buzz' href='/'>Buzz</a>
+<a id='tour' href='/'>Tour</a>
+</span>
+</h1>
+<a id='Sign_in_1' href='/'></a>
 
+</div>
+</div>	
+<body id="container">
+<div id="main">
+<img id='the_logo' src='/wahalu/assets/images/logo/wahalu_logo.png'>
+</div>
 </body>	
 </html>
